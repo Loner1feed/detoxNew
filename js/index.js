9d44c1d58b7482123c1ad2f7ae25c3be78aaca46
Loner1feed <45013853+Loner1feed@users.noreply.github.com>
2021-05-16 17:19:57 +0300

    3333

--- js/index.js
+++ js/index.js
@@ -30,3 +30,18 @@ $('.header__toggler').click(function() {
     $('.header__toggler').toggleClass('header__toggler--open');
     $('.content').toggleClass('content--open');
 })
+
+
+$(".header__nav").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+});
